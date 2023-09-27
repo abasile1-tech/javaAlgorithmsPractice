8bc3bba5bec4a641ca61147e9db975f71f994e44
John <ashton.basile@gmail.com>
2023-09-27 10:51:38 +0100

    I implemented my own Node class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // built-in implementation of LinkedList
         LinkedList<String> linkedList = new LinkedList<>();
 
         // Add elements to the linked list
@@ -14,5 +16,20 @@ public class Main {
 
         // Print the linked list
         System.out.println(linkedList);
+
+        // My implementation of Node
+        Node head = new Node(7);
+        Node node2 = new Node(5);
+        Node node3 = new Node(3);
+
+        head.next = node2;
+        node2.next = node3;
+
+        Node current = head;
+
+        while (current != null) {
+            System.out.println(current.val);
+            current = current.next;
+        }
     }
 }
---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Node {
+    int val;
+    public Node next;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
