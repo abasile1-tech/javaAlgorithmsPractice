585cdb350827de1370ac11a4d92a545e93ef4a4c
John <ashton.basile@gmail.com>
2023-09-27 10:52:24 +0100

    I made the Node class a doubly linked list

--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -3,9 +3,11 @@ package org.example;
 public class Node {
     int val;
     public Node next;
+    public Node prev;
 
     public Node(int val) {
         this.val = val;
         this.next = null;
+        this.prev = null;
     }
 }
