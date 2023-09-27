8c8628ec8dd13e3644b1536022cf6c39e4334241
John <ashton.basile@gmail.com>
2023-09-27 11:48:20 +0100

    linearSearch vs binarySearch timing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,40 @@ package org.example;
 import java.util.LinkedList;
 
 public class Main {
-
-    static boolean linear_search(int[] nums, int numberToFind) {
+    static int linearSearch(int[] nums, int numberToFind) {
         for(int i = 0; i < nums.length; i++) {
+
             if(nums[i] == numberToFind) {
-                return true;
+                return i;
             }
         }
-        return false;
+        return -1;
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // If the target is found at the middle, return its index
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            // If the target is greater, ignore the left half
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+
+            // If the target is smaller, ignore the right half
+            else {
+                right = mid - 1;
+            }
+        }
+        // If the target is not found in the array, return -1
+        return -1;
     }
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -48,5 +74,26 @@ public class Main {
             System.out.println(current.val);
             current = current.prev;
         }
+
+        double start = System.nanoTime();
+        int[] numbers = new int[100_000_000]; // Create an array to hold 1,000,000 numbers
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1; // Populate the array with numbers from 1 to 1,000,000
+        }
+        int target = 99_000_000;
+
+        int result = binarySearch(numbers, target);
+
+        double end = (System.nanoTime() - start) / 1_000_000_000;
+
+        System.out.println("Algorithm took " + end);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+
+
     }
 }
