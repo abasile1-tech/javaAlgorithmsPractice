61e328ff9eee01fd9cf2a07739a4a4144071b3d9
John <ashton.basile@gmail.com>
2023-09-27 10:40:17 +0100

    A linked list of strings

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        // Add elements to the linked list
+        linkedList.add("Element 1");
+        linkedList.add("Element 2");
+        linkedList.add("Element 3");
+
+        // Print the linked list
+        System.out.println(linkedList);
     }
 }
